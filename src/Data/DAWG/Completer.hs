370dc2db3d2a61d962bd5e9d4ac2fc4a47b3923d
Andrey Prokopenko <persiantiger@yandex.ru>
2025-08-12 21:08:44 +0200

    doc: add basic usage for Completer

--- src/Data/DAWG/Completer.hs
+++ src/Data/DAWG/Completer.hs
@@ -6,7 +6,9 @@ License: BSD-3-Clause
 Stability: experimental
 -}
 module Data.DAWG.Completer
-  ( Completer (..)
+  ( -- * Completer
+    -- $usage
+    Completer (..)
   , new
   , start
   , next
@@ -16,3 +18,24 @@ module Data.DAWG.Completer
   ) where
 
 import Data.DAWG.Internal.Completer
+
+-- $usage
+--
+-- The most basic scenario is to get all completions by given 'Data.DAWG.Dictionary.Dictionary' and 'Data.DAWG.Guide.Guide'. Consider following lexicon:
+--
+-- @
+--   a
+--   an
+--   and
+--   appear
+--   apple
+--   bin
+--   can
+--   cat
+-- @
+--
+-- Once dictionary and guide are ready, call 'completeKeys':
+--
+-- >>> completeKeys "a" dict guide
+-- ["a", "an", "and", "appear", "apple"]
+--
